8e9dff5fa0197e994408ebe77b4a69f3d459a920
budturfays <na7sa@hotmail.com>
2024-09-20 15:17:12 +0700

    Delete poolians directory

--- poolians/DirectXOverlay.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-bool InitializeOverlay();
-void StartOverlay();
-void StopOverlay();
-void UpdateOverlay(int* balls, int selected, int pocket, int combinations, float targetAngle, float currentAngle, bool editingMultiple);
-void UpdateOverlayText(const char* text);
--- poolians/SharedData.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <mutex>
-
-// Declare the variables as extern to be shared across files
-extern float targetBallPos[3];
-extern float pocketPos[3];
-extern std::mutex positionMutex;
